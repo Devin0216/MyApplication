0792dfaeb8477abc9f5e8de41d4c496c74cdb0e9
devin0216 <zhangdianwei02@126.com>
2018-08-19 15:59:45 +0800

    add gender

--- app/src/main/java/com/example/zhangdianwei/myapplication/NewClass.java
+++ app/src/main/java/com/example/zhangdianwei/myapplication/NewClass.java
@@ -3,4 +3,21 @@ package com.example.zhangdianwei.myapplication;
 public class NewClass {
     private String name;
 
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
